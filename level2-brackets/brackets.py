2e276a4e98de331b5aa9a89a1f1a214b464d452a
Markus Hinsche <markus.hinsche@artory.com>
2017-06-27 09:52:19 +0200

    Solve brackets challenge

--- level2-brackets/brackets.py
+++ level2-brackets/brackets.py
@@ -1,3 +1,20 @@
-def brackets(expression):
-    # Add your solution here
+SIGNATURES = ['()', '{}', '[]']
+
+
+def drop_irrelevant_chars(s, chars):
+    return ''.join(list(filter(lambda x: x in chars, s)))
+
+
+def rec(expression):
+    if not expression:
+        return True
+    for signature in SIGNATURES:
+        idx = expression.find(signature)
+        if idx != -1:
+            return rec(expression[0:idx] + expression[idx+2:])
     return False
+
+
+def brackets(expression):
+    expression = drop_irrelevant_chars(expression, ''.join(SIGNATURES))
+    return rec(expression)
